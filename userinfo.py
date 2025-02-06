65eec260bb52c3441d32cf7f7868896f0e401628
advtarun8 <advocatetarun89@gmail.com>
2025-02-06 23:57:35 +0530

    commit

---
+++ userinfo.py
@@ -0,0 +1,13 @@
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Print out a greeting message with the user's name
+print("Hello, " + name + "!")
+
+# Conditional statements
+age = int(input("How old are you? "))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
